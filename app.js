f288ddc2907f7d7197a317bb1892e09fde3c1979
Steve Moritani <steve.hayato.m@gmail.com>
2022-06-25 17:07:40 -0700

    week2 workshop

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//making localhost:3000 equal = path.join(__dirname, "public")
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
