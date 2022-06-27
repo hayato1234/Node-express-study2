ad62037d5a6d88783201adffa4705ebcdf6c553e
Steve Moritani <steve.hayato.m@gmail.com>
2022-06-27 15:30:09 -0700

    Basic Authentication

--- app.js
+++ app.js
@@ -35,7 +35,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//making localhost:3000 equal = path.join(__dirname, "public")
+//↓auth
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  //Buffer: global in node, no need to require
+  const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === "admin" && pass === "password") {
+    return next(); // authorized
+  } else {
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+}
+app.use(auth);
+
+//↓making localhost:3000 equal = path.join(__dirname, "public")
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
